874c01905270ff45078630f2749fb16b087971f6
Brian He <brian.he17@gmail.com>
2018-08-27 21:58:33 -0400

    comments on colors

--- Battle/classes/game.py
+++ Battle/classes/game.py
@@ -2,11 +2,11 @@ import random
 import time
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    HEADER = '\033[95m' #purple
+    OKBLUE = '\033[94m' #blue
+    OKGREEN = '\033[92m' #green
+    WARNING = '\033[93m' #yellow
+    FAIL = '\033[91m' #red
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
